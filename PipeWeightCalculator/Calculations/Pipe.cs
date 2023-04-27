5953df3055073d77d4013772557de86d2ab76f3f
Danipan497 <37379987+Danipan497@users.noreply.github.com>
2023-04-27 21:04:04 +0200

    Throw handling in output. App is running stable.
    As above.

--- PipeWeightCalculator/Calculations/Pipe.cs
+++ PipeWeightCalculator/Calculations/Pipe.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,25 +15,16 @@ namespace PipeWeightCalculator.Calculations
         double pi = Math.PI;
         public string CalculatePipeWeight(double pipeNominalDiameter, double wallThickness, double pipeLength, int material)
         {
-            //if (wallThickness > pipeNominalDiameter || wallThickness > pipeNominalDiameter / 2)
-            //{
-            //    //MessageBox.Show("Pipe wall thickness cannot be larger or equal to the half of the outside diameter! Incorrect action!");
-
-            //    return "";
-            //}
-            //else
-            //{
-            //    double pipeMassResult1 = Math.Pow((pipeNominalDiameter / 1000) / 2, 2);
-            //    double pipeMassResult2 = Math.Pow(((pipeNominalDiameter / 1000) - 2 * (wallThickness / 1000)) / 2, 2);
-            //    double pipeMassResult = (pi * (pipeMassResult1 - pipeMassResult2) * pipeLength) * material;
-
-            //    return pipeMassResult.ToString("N3");
-            //}
             try
             {
                 if (wallThickness > pipeNominalDiameter)
                 {
-                    return "";
+                    //return "";
+                    throw new WallThicknessBiggerThanPipeNominalDiameterException();
+
+                }
+                else if (wallThickness > pipeNominalDiameter / 2)
+                {
                     throw new WallThicknessBiggerThanHalfOfPipeNominalDiameterException();
                 }
                 else
@@ -46,26 +38,15 @@ namespace PipeWeightCalculator.Calculations
             }
             catch (WallThicknessBiggerThanPipeNominalDiameterException)
             {
+                return string.Empty;
                 throw new WallThicknessBiggerThanPipeNominalDiameterException("Pipe wall thickness cannot be larger than outside diameter! Incorrect action!");
-
+            }
+            catch (WallThicknessBiggerThanHalfOfPipeNominalDiameterException)
+            {
+                return string.Empty;
+                throw new WallThicknessBiggerThanHalfOfPipeNominalDiameterException("Pipe wall thickness cannot be larger than half of outside diameter! Incorrect action!");
             }
 
-            //if (wallThickness > pipeNominalDiameter)
-            //{
-            //    throw new WallThicknessBiggerThanPipeNominalDiameterException("Pipe wall thickness cannot be larger than outside diameter! Incorrect action!");
-            //}
-            //else if (wallThickness > pipeNominalDiameter / 2)
-            //{
-            //    throw new WallThicknessBiggerThanHalfOfPipeNominalDiameterException("Pipe wall thickness cannot be larger than half of outside diameter! Incorrect action!");
-            //}
-            //else
-            //{
-            //    double pipeMassResult1 = Math.Pow((pipeNominalDiameter / 1000) / 2, 2);
-            //    double pipeMassResult2 = Math.Pow(((pipeNominalDiameter / 1000) - 2 * (wallThickness / 1000)) / 2, 2);
-            //    double pipeMassResult = (pi * (pipeMassResult1 - pipeMassResult2) * pipeLength) * material;
-
-            //    return pipeMassResult.ToString("N3");
-            //}
         }
     }
 }
